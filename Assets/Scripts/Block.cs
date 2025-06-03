2ed7b16da6e4d10503d82ea9edec4c7283f0ba49
Ömer Kürşat Türköne <38076159+KursatTurkone@users.noreply.github.com>
2025-06-03 15:15:41 +0300

    Update Block.cs

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -8,34 +8,30 @@ public class Block : MonoBehaviour
     {
         blockData = data;
         
-        // SpriteRenderer ekleyip ayarlıyoruz
+   
         SpriteRenderer sr = gameObject.AddComponent<SpriteRenderer>();
         sr.sprite = data.blockSprite;
         sr.color = data.blockColor;
         
-        // Eğer sprite tanımlıysa sprite'ın gerçek boyutunu alıp ölçek hesaplaması yapıyoruz
+
         if(data.blockSprite != null)
         {
-            // Sprite'ın boyutları; import ayarlarında belirlenen Pixels Per Unit'a göre world unit cinsindedir.
+    
             Vector2 spriteSize = data.blockSprite.bounds.size;
             
-            // Eğer blok veri olarak istediğimiz boyutu tam olarak doldurmak istiyorsak,
-            // x ve y için ayrı ölçek faktörleri hesaplayabiliriz.
+    
             float scaleX = data.blockSize.x / spriteSize.x;
             float scaleY = data.blockSize.y / spriteSize.y;
-            
-            // Bu şekilde sprite, blockData'da belirlediğimiz boyuta tam oturur.
+     
             transform.localScale = new Vector3(scaleX, scaleY, 1f);
         }
         else
         {
-            // Sprite yoksa varsayılan ölçek kullanılır
+
             transform.localScale = Vector3.one;
         }
         
-        // BoxCollider2D ekliyoruz.
-        // Collider'ın size'sını sprite'ın orijinal boyutuna göre ayarlıyoruz, çünkü
-        // GameObject'in ölçeği (transform.localScale) collider'a da uygulanır.
+
         BoxCollider2D collider = gameObject.AddComponent<BoxCollider2D>();
         if (data.blockSprite != null)
         {
@@ -43,7 +39,7 @@ public class Block : MonoBehaviour
         }
         else
         {
-            // Sprite yoksa, collider'ı doğrudan blockSize'ya eşitleyebiliriz.
+         
             collider.size = data.blockSize;
         }
     }
